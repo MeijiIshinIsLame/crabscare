5545ff47c83d21aea3588881a909e604d1c0beb2
meijiishinislame <meijiishinislame@gmail.com>
2020-07-06 18:15:09 -1000

    added team base

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,9 +8,72 @@ include("shared.lua")
 AddCSLuaFile("round_controller/cl_round_controller.lua")
 include("round_controller/sv_round_controller.lua")
 
-function GM:PlayerSpawn(player)
-	player:SetMaxHealth(1) -- 1 hit kills
-	player:SetRunSpeed(500)
-	player:SetWalkSpeed(300)
-	player:Give("weapon_crowbar")
+function getGnomeLimit(how_many_humans)
+
+	return math.floor((how_many_humans + 1) / 4)
+
 end
+
+
+function GM:PlayerSpawn(ply)
+
+	ply:SetMaxHealth(1) -- 1 hit kills
+
+	-- if gnomes aint full u a gnome, buddy
+	if #team.GetPlayers(1) < getGnomeLimit(#team.GetPlayers(2)) then
+		print("you are a gnome")
+
+		ply:SetTeam(1)
+
+	else
+		print("you are a human")
+
+		ply:SetTeam(2)		
+
+	end
+
+
+	if ply:Team() == 1 then
+ 
+        ply:SetRunSpeed(1000)
+		ply:SetWalkSpeed(800)
+		ply:Give("weapon_crowbar")
+ 
+    elseif ply:Team() == 2 then
+ 
+        ply:SetRunSpeed(500)
+		ply:SetWalkSpeed(300)
+		ply:Give("weapon_gravitygun")
+ 
+    end
+
+end
+
+--[[function GM:PlayerLoadout( ply )
+ 
+    if ply:Team() == 1 then
+ 
+        ply:Give("weapon_crowbar")
+ 
+    elseif ply:Team() == 2 then
+ 
+        ply:Give("weapon_gravitygun")
+ 
+    end
+ 
+end]]
+ 
+function team_1( ply )
+ 
+    ply:SetTeam( 1 ) --Make the player join team 1
+ 
+end
+ 
+function team_2( ply )
+ 
+    ply:SetTeam( 2 ) --Make the player join team 2
+
+end
+ 
+concommand.Add( "team_1", team_1 ) --Add the command to set the players team to team 1
+concommand.Add( "team_2", team_2 ) --Add the command to set the players team to team 2
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,7 +4,11 @@ GM.Email = "N/A"
 GM.Website = "N/A"
 
 DeriveGamemode("sandbox")
+team.SetUp( 1, "Gnomes", Color(125, 255, 125, 255) )
+team.SetUp( 2, "Humans", Color(125, 255, 255, 255) ) 
 
 function GM:initialize()
+
 	self.baseclass.Initialize(self)
+
 end
